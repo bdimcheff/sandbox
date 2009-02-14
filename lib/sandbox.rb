4f3cf92610a205544b0c7cf59943e4ab9ccd107e
Brandon Dimcheff <bdimchef-git@wieldim.com>
2009-02-14 00:59:48 -0500

    Refactored sandbox so it will work with rspec

--- lib/sandbox.rb
+++ lib/sandbox.rb
@@ -1,28 +1,44 @@
 require 'tmpdir'
 require 'sha1'
 
+# Represents a temporary sandbox for testing that relies on the
+# filesystem.
 class Sandbox
-  class << self
-    def play(&block)
-      path = generate_path
-      
-      FileUtils.mkdir_p(path)
+  attr_accessor :path
 
-      begin
-        yield path
-      ensure
-        FileUtils.rm_r(path)
-      end
-    end
+  # Executes the block and yields the path to the sandbox directory.
+  # Cleans up the sandbox after the block is complete.
+  def self.play(path = nil, &block)
+    sandbox = Sandbox.new(path)
     
-    def generate_path
-      File.join(Dir.tmpdir, sandbox_dir)
+    begin
+      yield sandbox.path
+    ensure
+      sandbox.close
     end
+  end
+
+  # Creates a new Sandbox with an optional path.  Generates a random
+  # path in Dir.tmpdir if path is unspecified.
+  def initialize(path = nil)
+    self.path = path || generate_path
     
-    def sandbox_dir
-      sha = Digest::SHA1.hexdigest("--#{rand(20000)}---#{Time.now}--")
-      
-      "sandbox-#{sha}"
-    end
+    FileUtils.mkdir_p(self.path)
+  end
+  
+  # Cleans up the sandbox by removing the path
+  def close
+    FileUtils.rm_r(path)
+  end
+
+  private
+  def generate_path
+    File.join(Dir.tmpdir, sandbox_dir)
+  end
+  
+  def sandbox_dir
+    sha = Digest::SHA1.hexdigest("--#{rand(20000)}---#{Time.now}--")
+    
+    "sandbox-#{sha}"
   end
 end
--- spec/sandbox_spec.rb
+++ spec/sandbox_spec.rb
@@ -1,24 +1,11 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe "Sandbox" do
-  it "returns a path in the tmp directory" do
-    flexmock(Sandbox, :sandbox_dir => 'foo')
-    flexmock(Dir, :tmpdir => '/bar')
-
-    Sandbox.generate_path.should == '/bar/foo'
-  end
-
-  it "returns different paths for subsequent calls to sandbox_dir" do
-    dir1 = Sandbox.sandbox_dir
-    dir2 = Sandbox.sandbox_dir
-
-    dir1.should_not == dir2
-  end
-  
   context "no errors" do
     it "cleans up after itself" do
       path = Sandbox.play do |p|
         File.exist?(p).should be_true
+        File.ftype(p).should == 'directory'
         p
       end
 
@@ -37,6 +24,17 @@ describe "Sandbox" do
       }.should raise_error
 
       File.exist?(@path).should be_false
+    end
+  end
+
+  context "optional path supplied" do
+    it "uses the specified path" do
+      tmp = Dir.tmpdir
+      path = File.join(tmp, 'sandbox-test')
+      
+      Sandbox.play(path) do |p|
+        p.should == path
+      end
     end
   end
 end
